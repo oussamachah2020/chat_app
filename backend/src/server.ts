c9dc25e011ced840750d6b15d8ec5a95da12d0f8
Oussama CHAHIDI <oussama@ismaeland.com>
2023-06-07 22:26:48 +0100

    SETUP

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,16 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
+
+//--CORS MIDDLEWARE--
+app.use(cors());
+
+//--ROUTES--
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
